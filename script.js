c6db6d2c42f1f95ef73c1e154310eaeeee03eb4a
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-31 09:28:15 -0700

    started the java and ajax calls for the weather API

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var searchHistory = []
+var searchHistory = [];
+var APIkey = '&APPID=daa111366ea26bccb168e3feed86a063'
 
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=homer'; 
+$.ajax({
+    url : queryURL,
+    method : "GET" 
+}).then(function(response){
+console.log(response)})
